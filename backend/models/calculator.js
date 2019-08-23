1fceec6a5013be84b2e4e7eb35e161d5139df529
keithdhd <keith@keithdouglas.co.uk>
2019-08-23 20:02:58 +0100

    first try adding monthly contribs

--- backend/models/calculator.js
+++ backend/models/calculator.js
@@ -1,13 +1,26 @@
 class Calculator{
 
-  static calculateCompoundInterest({ baseAmount, annualInterest, calculationPeriod }){
-    annualInterest /= 100;
+  // static calculateCompoundInterest({ baseAmount, annualInterest, calculationPeriod }){
+  //   annualInterest /= 100;
+  //   const months = calculationPeriod * 12;
 
-    const totalInterest = baseAmount * (Math.pow(1 + annualInterest/12, 12*calculationPeriod) - 1);
-    const futureValue = totalInterest + baseAmount;
-    return Number(futureValue.toFixed(2));
+  //   const totalInterest = baseAmount * (Math.pow(1 + annualInterest/12, months) - 1);
+  //   const futureValue = totalInterest + baseAmount;
+  //   return Number(futureValue.toFixed(2));
+  // }
+
+  static calculateCompoundInterest({ baseAmount, annualInterest, calculationPeriod, monthlyContribution }){
+    const monthlyRate = annualInterest / 12;
+    const monthsToGrow = calculationPeriod * 12;
+
+    for(var i = 0; i < monthsToGrow; i++) {
+      baseAmount = baseAmount * (1 + monthlyRate) + monthlyContribution;
+    }
+    return Math.round(baseAmount * 100) / 100;
   }
 
 }
 
 module.exports = Calculator;
+
+// https://hassett.blog/2017/01/11/a-javascript-compound-interest-calculator/
--- backend/models/specs/calculator.test.js
+++ backend/models/specs/calculator.test.js
@@ -8,35 +8,35 @@ describe('Calculator: Compound interest', () => {
 
   test('should return 732.81 given 100, 10 percent, 20 years', () => {
     const result = Calculator.calculateCompoundInterest(
-      { baseAmount: 100, annualInterest: 10, calculationPeriod: 20 }
+      { baseAmount: 100, annualInterest: 10, calculationPeriod: 20, monthlyContribution: 0 }
     );
     expect(result).toBe(732.81);
   });
 
   test('should return 181.94 given 100, 6 percent, 10 years', () => {
     const result = Calculator.calculateCompoundInterest(
-      { baseAmount: 100, annualInterest: 6, calculationPeriod: 10 }
+      { baseAmount: 100, annualInterest: 6, calculationPeriod: 10, monthlyContribution: 0 }
     );
     expect(result).toBe(181.94);
   });
 
   test('should return 149,058.55 given 100000, 5 percent, 8 years', () => {
     const result = Calculator.calculateCompoundInterest(
-      { baseAmount: 100000, annualInterest: 5, calculationPeriod: 8 }
+      { baseAmount: 100000, annualInterest: 5, calculationPeriod: 8, monthlyContribution: 0 }
     );
     expect(result).toBe(149058.55);
   });
 
   test('should return 0.00 given 0, 10 percent, 1 year', () => {
     const result = Calculator.calculateCompoundInterest(
-      { baseAmount: 0, annualInterest: 10, calculationPeriod: 1 }
+      { baseAmount: 0, annualInterest: 10, calculationPeriod: 1, monthlyContribution: 0 }
     );
     expect(result).toBe(0.00);
   });
 
   test('should return 100.00 given 100, 0 percent, 10 years', () => {
     const result = Calculator.calculateCompoundInterest(
-      { baseAmount: 100, annualInterest: 0, calculationPeriod: 10 }
+      { baseAmount: 100, annualInterest: 0, calculationPeriod: 10, monthlyContribution: 0 }
     );
     expect(result).toBe(100.00);
   });
