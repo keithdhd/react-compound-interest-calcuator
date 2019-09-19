97a0ac3f8794310172db9cfc553912859034149c
Keith Douglas <keith.douglas@codeclan.com>
2019-09-19 16:57:04 +0100

    adds monthly contributions

--- backend/models/calculator.js
+++ backend/models/calculator.js
@@ -1,26 +1,23 @@
-class Calculator{
-
-  // static calculateCompoundInterest({ baseAmount, annualInterest, calculationPeriod }){
-  //   annualInterest /= 100;
-  //   const months = calculationPeriod * 12;
-
-  //   const totalInterest = baseAmount * (Math.pow(1 + annualInterest/12, months) - 1);
-  //   const futureValue = totalInterest + baseAmount;
-  //   return Number(futureValue.toFixed(2));
-  // }
-
-  static calculateCompoundInterest({ baseAmount, annualInterest, calculationPeriod, monthlyContribution }){
+class Calculator {
+  static calculateCompoundInterest({
+    baseAmount,
+    annualInterest,
+    calculationPeriod,
+    monthlyContribution
+  }) {
+    annualInterest /= 100;
     const monthlyRate = annualInterest / 12;
     const monthsToGrow = calculationPeriod * 12;
+    baseAmount += monthlyContribution;
 
-    for(var i = 0; i < monthsToGrow; i++) {
+    for (var i = 0; i < monthsToGrow; i++) {
       baseAmount = baseAmount * (1 + monthlyRate) + monthlyContribution;
     }
-    return Math.round(baseAmount * 100) / 100;
-  }
 
+    baseAmount -= monthlyContribution;
+
+    return Number(baseAmount.toFixed(2));
+  }
 }
 
 module.exports = Calculator;
-
-// https://hassett.blog/2017/01/11/a-javascript-compound-interest-calculator/
